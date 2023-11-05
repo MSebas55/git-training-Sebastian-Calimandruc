4ca4c3dbfa42be61aec118e7566ae3ce43cec11a
MSebas55 <sebas@MaquinaSebas.myguest.virtualbox.org>
2023-11-05 23:55:54 +0100

    Ordered HiloCaraCruz.java

--- src/HiloCaraCruz.java
+++ src/HiloCaraCruz.java
@@ -1,2 +1,15 @@
-public class HiloCaraCruz {
+import java.util.Random;
+public class HiloCaraCruz extends Thread {
+    private boolean cara;
+    public HiloCaraCruz() {
+    }
+    public void run() {
+        cara = new Random().nextBoolean();
+    }
+    public boolean isCara() {
+        return cara;
+    }
+    public void setCara(boolean cara) {
+        this.cara = cara;
+    }
 }
