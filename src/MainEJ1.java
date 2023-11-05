aba41454a35cc464597f1592fd90536de9c47524
MSebas55 <sebas@MaquinaSebas.myguest.virtualbox.org>
2023-11-05 23:43:08 +0100

    Started MainEj1.java

--- src/MainEJ1.java
+++ src/MainEJ1.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class MainEJ1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n, contadorCaras = 0, contadorCruces = 0;
+        //Array que usaremos una vez creados los hilos
+        //ArrayList<HiloCaraCruz> hilos = new ArrayList<HiloCaraCruz>();
+
+        System.out.print("Introduce la cantidad de hilos: ");
+        n = scanner.nextInt();
+        scanner.close();
+    }
 }
